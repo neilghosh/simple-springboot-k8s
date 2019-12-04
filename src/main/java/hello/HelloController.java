1235582085a49666483c420ec17319262891b583
Sabyasachi Ghosh <sabyasachi.ghosh@salesforce.com>
2019-12-04 22:47:37 +0530

    Stop logging bean Big font response

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "Hello GDG Hyderabad";
+		return "<font size=48 color=red>Hello GDG Hyderabad V1</font>";
 	}
 	
 }
